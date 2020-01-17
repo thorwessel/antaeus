da0135a998fe35a4aff9ebe52fc4ba8ed746dbd2
thorwessel <thor@pleo.io>
2020-01-17 20:48:01 +0100

    Added test class for BillingService

---
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -0,0 +1,10 @@
+package io.pleo.antaeus.core.services
+
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class BillingServiceTest {
+
+}
