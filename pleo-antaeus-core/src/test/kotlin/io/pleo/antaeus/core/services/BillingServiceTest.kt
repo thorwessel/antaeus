5dd4b3baa315dfeaf2875c31ab0d57b482dd3241
thorwessel <thor@pleo.io>
2020-01-17 21:29:24 +0100

    Updated test

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -7,17 +7,39 @@ import io.mockk.mockk
 import io.mockk.runs
 import io.mockk.verify
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.models.Money
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
 
 class BillingServiceTest {
 
     private val paymentProvider = mockk<PaymentProvider>()
-    private val billingService = BillingService(paymentProvider)
+    private val invoiceService = mockk<InvoiceService>()
+
+    private val billingService = BillingService(
+        paymentProvider,
+        invoiceService
+    )
+
+    private val pendingInvoice = Invoice(
+        id = 1,
+        customerId = 1,
+        amount = Money(
+            value = BigDecimal(999),
+            currency = Currency.EUR
+        ),
+        status = InvoiceStatus.PENDING
+    )
 
     @Test
     fun `runBilling will call payment provider charge`() {
         every { paymentProvider.charge(any()) } returns true
+        every { invoiceService.fetchAllPending() } returns listOf(pendingInvoice)
+
 
         billingService.runBilling()
 
