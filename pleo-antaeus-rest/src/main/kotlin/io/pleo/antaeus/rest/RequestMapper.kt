f91ba575219690e3d934deef8e22bf0d4816fa14
thorwessel <thor@pleo.io>
2020-01-20 19:26:21 +0100

    Added comment

--- pleo-antaeus-rest/src/main/kotlin/io/pleo/antaeus/rest/RequestMapper.kt
+++ pleo-antaeus-rest/src/main/kotlin/io/pleo/antaeus/rest/RequestMapper.kt
@@ -2,6 +2,7 @@ package io.pleo.antaeus.rest
 
 import io.pleo.antaeus.models.InvoiceStatus
 
+// Was not able to find a better name, but I wanted to include a simple framework which could be used with other requests
 object RequestMapper {
     fun mapStatus(status: String?): InvoiceStatus? {
         return if (InvoiceStatus.values().any { it.name == status }) {
